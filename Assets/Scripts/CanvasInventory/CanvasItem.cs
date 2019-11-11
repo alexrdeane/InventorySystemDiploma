b8491bf1f8629ce3938b4bcfebfe0e3be711f36a
Alex Deane <alexrdeane@hotmail.com>
2019-11-11 23:42:24 +1100

    canvas icons and scripts

---
+++ Assets/Scripts/CanvasInventory/CanvasItem.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class CanvasItem
+{
+    #region Variables
+    private int _id;
+    private string _name;
+    private string _description;
+    private int _amount;
+    private int _value;
+    private CanvasItemType _canvasType;
+    private Texture2D _icon;
+    private GameObject _mesh;
+    private bool _stackable;
+    private bool _isStacked;
+    #endregion
+    #region Public Properties
+    public int ID
+    {
+        get { return _id; }//read
+        set { _id = value; }//write
+    }
+    public string Name
+    {
+        get { return _name; }//read
+        set { _name = value; }//write
+    }
+    public string Description
+    {
+        get { return _description; }//read
+        set { _description = value; }//write
+    }
+    public int Value
+    {
+        get { return _value; }//read
+        set { _value = value; }//write
+    }
+    public int Amount
+    {
+        get { return _amount; }//read
+        set { _amount = value; }//write
+    }
+    public GameObject ItemMesh
+    {
+        get { return _mesh; }
+        set { _mesh = value; }
+
+    }
+    public bool Stackable
+    {
+        get { return _stackable; }//read
+        set { _stackable = value; }//write
+    }
+    public bool IsStacked
+    {
+        get { return _isStacked ; }//read
+        set { _isStacked = value; }//write
+    }
+    public Texture2D Icon
+    {
+        get { return _icon; }
+        set { _icon = value; }
+    }
+    public CanvasItemType Type
+    {
+        get { return _canvasType; }
+        set { _canvasType = value; }
+        #endregion
+    }
+}
+public enum CanvasItemType
+{
+    All,
+    Ingredients,
+    Ammo,
+    NumberOfTypes
+}
---
+++ Assets/Scripts/CanvasInventory/CanvasItemData.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CanvasItemData
+{
+    public static Item CreateItem(int itemID)
+    {
+        int _amount = 0;
+        int _value = 0;
+        int _damage = 0;
+        int _armour = 0;
+        int _heal = 0;
+        int _durability = 0;
+        int _mana = 0;
+        string _icon = "";
+        string _mesh = "";
+        bool _stackable;
+        bool _isStacked = false;
+        string _name = "";
+        string _description = "";
+        string _rarity = "";
+        CanvasItemType _canvasType;
+        switch (itemID)
+        {
+            #region Ingredient 0 - 10
+            case 0:
+                _name = "Apple";
+                _description = "A fruit that heals or fuels your body";
+                _amount = 1;
+                _value = 1;
+                _canvasType = CanvasItemType.Ingredients;
+                _icon = "Ingredients/Apple";
+                _mesh = "Ingredients/Apple";
+                _damage = 0;
+                _armour = 0;
+                _heal = 2;
+                _rarity = "common";
+                _stackable = true;
+                _isStacked = false;
+                break;
+            #endregion
+            #region Ammo 11 - 20
+            case 11:
+                _name = "Pistol Bullet";
+                _description = "A small bullet that is cheap but packs a punch";
+                _amount = 1;
+                _value = 1;
+                _canvasType = CanvasItemType.Ammo;
+                _icon = "Ammo/Pistol";
+                _mesh = "Ammo/Pistol";
+                _damage = 0;
+                _armour = 0;
+                _heal = 2;
+                _rarity = "common";
+                _stackable = true;
+                _isStacked = false;
+                break;
+            #endregion
+            default:
+                _name = "default";
+                _description = "default item";
+                _amount = 1;
+                _value = 1;
+                _canvasType = CanvasItemType.Ingredients;
+                _icon = "Ingredients/Apple";
+                _mesh = "Ingredients/Apple";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                _rarity = "common";
+                _stackable = true;
+                _isStacked = false;
+                break;
+        }
+
+        Item temp = new Item
+        {
+            ID = itemID,
+            Value = _value,
+            Amount = _amount,
+            Icon = Resources.Load("Icons/" + _icon) as Texture2D,
+            ItemMesh = Resources.Load("Meshes/" + _mesh) as GameObject,
+            Name = _name,
+            Description = _description,
+            Stackable = _stackable       
+    };
+        return temp;
+    }
+}
+
